1f1bc60637a8d65b3fc42a7fb6f9f691745b2f45
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-02-27 10:57:08 +1000

    INITIAL COMMIT

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ContextProvider } from './Context/ContextProvider';
+
+import Navigation from './Navigation/Navigation';
+import Brochure from './Routes/Brochure/Brochure';
+import StoreFront from './Routes/StoreFront/StoreFront';
+import Cart from './Routes/Cart/Cart';
+import Tail from './Tail/Tail';
+
+const rootElement: HTMLElement | null = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ContextProvider>
+        <Router>
+          <Navigation />
+          <Routes>
+            <Route path="/" Component={Brochure}/>
+            <Route path="/StoreFront" Component={StoreFront}/>
+            <Route path="/Cart" Component={Cart}/>            
+          </Routes>
+        </Router>
+        <Tail />
+      </ContextProvider>
+    </React.StrictMode>,
+    rootElement
+  );
+}
