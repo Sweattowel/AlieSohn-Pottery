a19fab178651a0336e7201c09684d58762b672ee
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-04-15 15:23:42 +1000

    idk what even happened, moved vscode and new m_d formed

--- Server/CSharpServer/Server.cs
+++ Server/CSharpServer/Server.cs
@@ -12,9 +12,12 @@ namespace Server
 {
     public class Program
     {
+        // ONSTARTUP CALLS
         public static void Main(string[] args)
         {
+            Timer timer = new Timer(UpdateBrochure, null, TimeSpan.Zero, TimeSpan.FromMinutes(30));
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -25,7 +28,15 @@ namespace Server
                             .UseUrls("http://0.0.0.0:5000");
                 });
     }
-
+    // method for getting database variables
+    private static string GetConnectionString()
+    {
+        string userStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_USER");
+        string passStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_PASSWORD");
+        string hostStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_HOST");
+        string dataBaseStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_DATABASE");
+        return $"Server={hostStr};Database={dataBaseStr};User Id={userStr};Password={passStr};";
+    }
     public class Startup
     {
         public IConfiguration Configuration { get; }
@@ -61,141 +72,155 @@ namespace Server
             });
         }
     }
-
-    namespace Server.Controllers
+        ////////////////// BROCHURE HANDLE    
+        // BROCHURE DEFINTION 
+    public static class BrochureStorage
     {
-        [Route("/api/storeItems")]
-        [ApiController]
-        public class StoreItemsController : ControllerBase
+        public static List<BrochureItem> Brochure { get; set; }
+    }
+    public class BrochureItem
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string ImagePath { get; set; }
+        public int OrderCount { get; set; }
+    }
+        // BROCHURE UPDATE CALL
+        private static void UpdateBrochure(object state)
         {
-            public class StoreItem
+            try
             {
-                public int ItemID { get; set; }
-                public string ItemName { get; set; }
-                public decimal ItemPrice { get; set; }
-                public string ImagePath { get; set; }
-                public string ItemDescription { get; set; }
+                Console.WriteLine("Updating Brochure");
+                CreateBrochure();
             }
-
-            [HttpGet]
-            public ActionResult<IEnumerable<StoreItem>> GetStoreItems()
+            catch (Exception ex)
             {
-                Console.WriteLine("Received Request for storeItems");
+                Console.WriteLine($"Error updating brochure: {ex.Message}");
+            }
+        }
+        // BROCHURE CREATE
+        private static void CreateBrochure()
+        {
+            try
+            {
+                List<BrochureItem> brochure = new List<BrochureItem>();
                 string connectionString = GetConnectionString();
-                string queryStatement = "SELECT * FROM storeItems";
-                List<StoreItem> storeItems = new List<StoreItem>();
+                string queryStatement = "SELECT storeItems.itemID, storeItems.itemName, storeItems.imagePath, storeItems.itemPrice, COUNT(orders.orderID) AS order_count FROM storeItems JOIN orders ON storeItems.itemID = orders.itemID GROUP BY storeItems.itemID, storeItems.itemName ORDER BY order_count DESC LIMIT 3;";
 
-                try
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    using (SqlCommand command = new SqlCommand(queryStatement, connection))
                     {
-                        using (SqlCommand command = new SqlCommand(queryStatement, connection))
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
                         {
-                            connection.Open();
-                            SqlDataReader reader = command.ExecuteReader();
-
-                            while (reader.Read())
+                            BrochureItem item = new BrochureItem
                             {
-                                StoreItem item = new StoreItem
-                                {
-                                    ItemID = reader.GetInt32(reader.GetOrdinal("itemID")),
-                                    ItemName = reader.GetString(reader.GetOrdinal("itemName")),
-                                    ItemPrice = reader.GetDecimal(reader.GetOrdinal("itemPrice")),
-                                    ImagePath = reader.GetString(reader.GetOrdinal("imagePath")),
-                                    ItemDescription = reader.GetString(reader.GetOrdinal("itemDescription"))
-                                };
-                                storeItems.Add(item);
-                            }
-
-                            reader.Close();
+                                ItemID = reader.GetInt32(reader.GetOrdinal("itemID")),
+                                ItemName = reader.GetString(reader.GetOrdinal("itemName")),
+                                ItemPrice = reader.GetDecimal(reader.GetOrdinal("itemPrice")),
+                                ImagePath = reader.GetString(reader.GetOrdinal("imagePath")),
+                                OrderCount = reader.GetInt32(reader.GetOrdinal("order_count"))
+                            };
+                            brochure.Add(item);
                         }
+                        reader.Close();
                     }
-
-                    return Ok(storeItems);
                 }
-                catch (SqlException ex)
-                {
-                    return StatusCode(500, $"Database Error: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest($"Error: {ex.Message}");
-                }
+                BrochureStorage.Brochure = brochure;
             }
-
-            private string GetConnectionString()
+            catch (Exception ex)
             {
-                string userStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_USER");
-                string passStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_PASSWORD");
-                string hostStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_HOST");
-                string dataBaseStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_DATABASE");
-                return $"Server={hostStr};Database={dataBaseStr};User Id={userStr};Password={passStr};";
+                Console.WriteLine($"Error creating brochure: {ex.Message}");
+            }
+        }
+}
+namespace Server.Controllers
+{
+    //////// ROUTES
+    // GET STOREITEMS
+    [Route("/api/storeItems")]
+    [ApiController]
+    public class StoreItemsController : ControllerBase
+    {
+        public class StoreItem
+        {
+            public int ItemID { get; set; }
+            public string ItemName { get; set; }
+            public decimal ItemPrice { get; set; }
+            public string ImagePath { get; set; }
+            public string ItemDescription { get; set; }
+        }
+    [HttpGet]
+        public ActionResult<IEnumerable<StoreItem>> GetStoreItems()
+        {
+            Console.WriteLine("Received Request for storeItems");
+            string connectionString = GetConnectionString();
+            string queryStatement = "SELECT * FROM storeItems";
+            List<StoreItem> storeItems = new List<StoreItem>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(queryStatement, connection))
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                    
+                        while (reader.Read())
+                        {
+                            StoreItem item = new StoreItem
+                            {
+                                ItemID = reader.GetInt32(reader.GetOrdinal("itemID")),
+                                ItemName = reader.GetString(reader.GetOrdinal("itemName")),
+                                ItemPrice = reader.GetDecimal(reader.GetOrdinal("itemPrice")),
+                                ImagePath = reader.GetString(reader.GetOrdinal("imagePath")),
+                                ItemDescription = reader.GetString(reader.GetOrdinal("itemDescription"))
+                            };
+                            storeItems.Add(item);
+                        }
+                        reader.Close();
+                    }
+                }
+                return Ok(storeItems);
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(500, $"Database Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
             }
         }
     }
-        [Route("/api/getBrochure")]
-        [ApiController]
-        public class GetBrochureController:  ControllerBase 
+    
+    // GET PREMADE BROCHURE
+    [Route("/api/getBrochure")]
+    [ApiController]
+    public class GetBrochureController:  ControllerBase 
+    {
+        [HttpGet]
+        public  ActionResult<IEnumerable<BrochureItem>> GetBrochure()
         {
-            public class BrochureItem
+            Console.WriteLine("Received Request for Brochure");
+            string connectionString = GetConnectionString();
+            try
             {
-                public int ItemID { get; set; };
-                public string itemName { get; set; };
-                public string imagePath { get; set; };
-                public decimal itemPrice { get; set; };
-                public int order_count { get; set; };
+                return Ok(BrochureStorage.Brochure)
             }
-            [HttpGet]
-            public  ActionResult<IEnumerable<BrochureItem>> GetBrochure()
+            catch (SqlException ex)
             {
-                Console.WriteLine("Received Request for Brochure");
-                string connectionString = GetConnectionString();
-                string queryStatement = "SELECT storeItems.itemID, storeItems.itemName, storeItems.imagePath, storeItems.itemPrice, COUNT(orders.orderID) AS order_count FROM storeItems JOIN orders ON storeItems.itemID = orders.itemID GROUP BY storeItems.itemID, storeItems.itemName ORDER BY order_count DESC LIMIT 3;";
-                
-                List<BrochureItem> brochure = new List<BrochureItem>();
-
-                try
-                {
-                    using (SqlConnection connect = new SqlConnection(connectionString))
-                    {
-                        using (SqlCommand command = new SqlCommand(queryStatement, connection))
-                        {
-                            connection.Open();
-                            SqlDataReader reader = command.ExecuteReader();
-
-                            while (reader.Read())
-                            {
-                                BrochureItem item = new BrochureItem
-                                {
-                                        ItemID = reader.GetInt32(reader.GetOrdinal("itemID")),
-                                        ItemName = reader.GetString(reader.GetOrdinal("itemName")),
-                                        ItemPrice = reader.GetDecimal(reader.GetOrdinal("itemPrice")),
-                                        ImagePath = reader.GetString(reader.GetOrdinal("imagePath")),
-                                        order_count = reader.GetInt32(reader.GetOrdinal("order_count")) 
-                                }
-                                brochure.Add(item)
-                            }
-                            reader.Close()
-                        }
-                    }
-                    return Ok(brochure)
-                }
-                catch (SqlException ex)
-                {
-                    return StatusCode(500, $"Database Error: {ex.Message}");
-                }
+                return StatusCode(500, $"Database Error: {ex.Message}");
+            }
                 catch (Exception ex)
-                {
-                    return BadRequest($"Error: {ex.Message}");
-                }
+            {
+                return BadRequest($"Error: {ex.Message}");
             }
-            private string GetConnectionString()
-                {
-                    string userStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_USER");
-                    string passStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_PASSWORD");
-                    string hostStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_HOST");
-                    string dataBaseStr = Environment.GetEnvironmentVariable("REACT_APP_DATABASE_DATABASE");
-                    return $"Server={hostStr};Database={dataBaseStr};User Id={userStr};Password={passStr};";
-                }
         }
+    }
 }
