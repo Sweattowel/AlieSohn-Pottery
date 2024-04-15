adac46fb93b2d44dff942b46bea01a04279ed489
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-04-15 16:02:07 +1000

    talking about the ;!!!!!

--- Server/CSharpServer/Server.cs
+++ Server/CSharpServer/Server.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Threading
+using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
